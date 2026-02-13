1ce5059e7bfb1bb0f94bba36468167f0c9e349b2
Guilherme Antonio Santos de Oliveira <guilhermegaso420@gmail.com>
2026-02-13 19:23:52 -0300

    Subindo codico base

---
+++ back/reserva-tech/src/components/Reserva.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './Reserva.css'
+
+function Reserva() {
+
+    return (
+        <>
+           <div className='container'>
+                <main>
+                    <div>teste</div>
+                </main>
+           </div>
+        </>
+    )
+
+}
+
+export default Reserva
