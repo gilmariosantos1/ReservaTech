c8ca30636f07f75a949f15f21bdbcb7a32394c96
otavioou <otaviooliveiradasilva788@gmail.com>
2026-02-25 21:35:46 -0300

    Add files via upload

---
+++ src/components/home.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom'
+import './home.css'
+
+const Home = () => {
+    const navigate = useNavigate()
+
+    const handleIniciar = () => {
+        navigate('/homemenu')
+    }
+
+    return (
+        <>
+            <div className="centro">
+                <img src="/Imagens/Design sem nome (10).png" alt="ReservaTech Logo" />
+                <div className="button">
+                    <button onClick={handleIniciar} className="centro-button">
+                        Iniciar
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
