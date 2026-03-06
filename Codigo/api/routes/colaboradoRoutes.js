6b9758e13f8bae7e2a688013fbe0b841635432fe
Breno <nobresilva360@gmail.com>
2026-03-05 21:36:31 -0300

    quase terminado

--- Codigo/api/routes/colaboradoRoutes.js
+++ Codigo/api/routes/colaboradoRoutes.js
@@ -1,12 +1,90 @@
 import { Router } from 'express';
 import * as controllers from '../controllers/ColaboradorController.js';
-import { body } from 'express-validator';
+import { body, param} from 'express-validator';
 import auth from '../middlewares/validateRequest.js';
 
 const router = Router();
+const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
+const phoneRegex = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
 
 router.get('/', controllers.listar);
-router.get('/:id', controllers.obter);
-router.post('/', auth, [
-    
-])
+router.get('/:id', param('id').isInt({ min: 1 }), controllers.getById);
+router.post('/',
+  auth,
+  [
+    body('nome')
+      .trim()
+      .notEmpty()
+      .isLength({ min: 3, max: 120 }),
+
+    body('email')
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .normalizeEmail(),
+
+    body('telefone')
+      .trim()
+      .notEmpty()
+      .matches(phoneRegex),
+
+    body('cpf')
+      .trim()
+      .notEmpty()
+      .matches(cpfRegex),
+
+    body('dataNascimento')
+      .notEmpty()
+      .isISO8601(),
+
+    body('matricula')
+      .trim()
+      .notEmpty()
+      .isLength({ min: 3, max: 30 }),
+  ],
+  controllers.create
+);
+
+router.put('/:id',
+  auth,
+  [
+    body('nome')
+      .optional()
+      .trim()
+      .isLength({ min: 3, max: 120 }),
+
+    body('email')
+      .optional()
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+
+    body('telefone')
+      .optional()
+      .trim()
+      .matches(phoneRegex),
+
+    body('cpf')
+      .optional()
+      .trim()
+      .matches(cpfRegex),
+
+    body('dataNascimento')
+      .optional()
+      .isISO8601(),
+
+    body('matricula')
+      .optional()
+      .trim()
+      .isLength({ min: 3, max: 30 }),
+  ],
+  controllers.update
+);
+
+router.delete('/:id',
+  auth,
+  param('id').isInt({ min: 1 }),
+  controllers.remove
+);
+
+export default router;
